bd60b6c058b55ccad80796b3da5092ef0a43d93a
kaikisk <kueobei@gmail.com>
2019-05-17 04:34:48 +0900

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -49,8 +49,76 @@ self.addEventListener('fetch', function(event) {
         })
     );
 });
-*/
+
 self.addEventListener('fetch', function(event){});
+*/
+
+self.addEventListener('fetch', function(event) {
+  //ブラウザが回線に接続しているかをboolで返してくれる
+  var online = navigator.onLine;
+
+  //回線が使えるときの処理
+  if(online){
+    event.respondWith(
+      caches.match(event.request)
+        .then(
+        function (response) {
+          if (response) {
+            return response;
+          }
+          //ローカルにキャッシュがあればすぐ返して終わりですが、
+          //無かった場合はここで新しく取得します
+          return fetch(event.request)
+            .then(function(response){
+              // 取得できたリソースは表示にも使うが、キャッシュにも追加しておきます
+              // ただし、Responseはストリームなのでキャッシュのために使用してしまうと、ブラウザの表示で不具合が起こる(っぽい)ので、複製しましょう
+              cloneResponse = response.clone();
+              if(response){
+                //ここ&&に修正するかもです
+                if(response || response.status == 200){
+                  //現行のキャッシュに追加
+                  caches.open(CACHE_NAME)
+                    .then(function(cache)
+                    {
+                      cache.put(event.request, cloneResponse)
+                      .then(function(){
+                        //正常にキャッシュ追加できたときの処理(必要であれば)
+                      });
+                    });
+                }else{
+                  //正常に取得できなかったときにハンドリングしてもよい
+                  return response;
+                }
+                return response;
+              }
+            }).catch(function(error) {
+              //デバッグ用
+              return console.log(error);
+            });
+        })
+    );
+  }else{
+    //オフラインのときの制御
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // キャッシュがあったのでそのレスポンスを返す
+          if (response) {
+            return response;
+          }
+          //オフラインでキャッシュもなかったパターン
+          return caches.match("offline.html")
+              .then(function(responseNodata)
+              {
+                //適当な変数にオフラインのときに渡すリソースを入れて返却
+                //今回はoffline.htmlを返しています
+                return responseNodata;
+              });
+        }
+      )
+    );
+  }
+});
 
 const STATIC_CACHE_NAMES = [
     STATIC_CACHE_NAME
