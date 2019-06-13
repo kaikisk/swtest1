b5522b85d00952682cbfe87e920630b876da0a26
kaikisk <kueobei@gmail.com>
2019-06-14 08:08:04 +0900

    Update confirmDate.js

--- javaScript/confirmDate.js
+++ javaScript/confirmDate.js
@@ -1,20 +1,22 @@
-//var today = new Date();
+var today = new Date();
 
 setInterval(function(){
-    //var appointmentsString = localStorage.getItem('appointments');
-    //var appointments = JSON.parse(appointmentsString);
-    navigator.serviceWorker.ready.then(function(registration){
-        registration.showNotification('1日前だよ');
-    });
-
-    //for(var i = 0; i < appointments.length; i++){
-        //var date = appointments[i].dateClient.split('-');
-        //console.log('分割' +date.join(' & '));
-        //if(today.getFullYear == date[0] && today.getMonth+1 == date[1] 
-            //&& today.getDate == date[2]){
+    var appointmentsString = localStorage.getItem('appointments');
+    var appointments = JSON.parse(appointmentsString);
+    console.log(today);
+    
+    for(var i = 0; i < appointments.length; i++){
+        var date = appointments[i].dateClient.split('-');
+        console.log('分割' +date.join(' & '));
+        if(today.getFullYear == date[0] && today.getMonth+1 == date[1] 
+            && today.getDate == date[2]){
+                navigator.serviceWorker.ready.then(function(registration){
+                    registration.showNotification('1日前だよ');
+                });
+                //下の書き方はなぜかスマホに対応してない
                //var n = new Notification('1日前です',appointments[i].dateClient);
                //setTimeout(n.close.bind(n), 20000); 
-        //}
-    //}
-}, 86400000);
+        }
+    }
+}, 10000);
 //86400000mm = 1日
