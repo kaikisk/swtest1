50f4d50f63acac9a0563dae994e26928eea2c792
kaikisk <kueobei@gmail.com>
2019-06-17 06:57:44 +0900

    Update notification.js

--- javaScript/notification.js
+++ javaScript/notification.js
@@ -2,4 +2,8 @@ Notification.requestPermission(function(result) {
   if(result === 'granted') {
     console.log("通知が許可されています");
   }
+  else if(result === 'denied'){
+    console.log("通知が拒否されています");
+  }
+  else if(result === 'default')
 });
